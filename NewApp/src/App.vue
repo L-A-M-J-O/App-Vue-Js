ade08d29b53117e9a49e323a177ba3d95f48e9f4
L-A-M-J-O <lamjo30@gmail.com>
2022-12-17 19:13:44 +0100

    push

--- NewApp/src/App.vue
+++ NewApp/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import ListaVue from './components/Lista.vue';
 </script>
 
 <template>
@@ -13,7 +14,8 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <ListaVue/>
+    <!-- <TheWelcome /> -->
   </main>
 </template>
 
---
+++ NewApp/src/components/Lista.vue
@@ -0,0 +1,18 @@
+<style>
+  @import '../styles/Lista.css';
+</style>
+<script>
+export default {
+  name: 'Lista',
+  data () {
+    return {
+      subtitulo: 'Soy el componente Lista'
+    }
+  }
+}
+</script>
+<template>
+  <section id="lista">
+    <h2><span>1.</span>{{subtitulo}}</h2>
+  </section>
+</template>
