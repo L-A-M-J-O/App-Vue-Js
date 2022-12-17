88cae7a991d5df145c62d70def50dd0cf71e8844
L-A-M-J-O <lamjo30@gmail.com>
2022-12-18 00:06:02 +0100

    push

--- NewApp/src/components/Eventos.vue
+++ NewApp/src/components/Eventos.vue
@@ -1,10 +1,33 @@
-<script setup>
- const handleClick = () => {
-  this.cambio = !this.cambio;
- }
+<script setup>;
+import {ref} from'vue';
+  const numeros = ref([]);
+  const verde = 'Color: green';
+  const rojo = 'Color: red'
+  let cambio = ref(0);
+  const increment = () => {
+    cambio.value ++;
+    console.log(cambio.value)
+  }
+  const desincrement = () => {
+    cambio.value --;
+    console.log(cambio.value)
+  }
+  const reset = () => {
+    cambio.value = 0;
+  }
+  const agregar = () => {
+    numeros.value.push(cambio.value)
+  }
+
 </script>
 <template>
-  <p v-show='handleClick'>{{cambio ? 'Autorizado' : 'No Autorizado'}}</p>
-  <button v-on:click="handleClick">Click On</button>
-  <button @click="handleClick">Click @</button>
+  <h1 v-if="cambio <= 0" :style="rojo">{{cambio}}</h1>
+  <h1 v-else :style="verde">{{cambio}}</h1>
+  <button @click="increment">Aumentar</button>
+  <button @click="desincrement">Descrementar</button>
+  <button @click="reset">Reset</button>
+  <button @click="agregar">Agregar</button>
+  <ul>
+    <li v-for="numero in numeros">Has Agregado {{numero}}</li>
+  </ul>
 </template>
