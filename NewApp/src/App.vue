760cbfeefc02e82206d05ea5d5158e5c93186287
Alan Naranjo <lamjo30@gmail.com>
2023-04-05 13:16:59 +0200

    A

--- NewApp/src/App.vue
+++ NewApp/src/App.vue
@@ -6,55 +6,24 @@ import VIf from './components/V-If.vue';
 import VOf from './components/V-of.vue';
 import Eventos from './components/Eventos.vue';
 import Pokemons from './components/Pokemons.vue';
+import Ancora from './components/Ancora.vue';
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <Lista/>
+    <Ancora/>
+    <!-- <Lista/>
     <hr/>
     <VIf/>
     <hr/>
     <VOf/>
     <hr/>
-    <Eventos/>
+    <Eventos/> -->
     <!-- <TheWelcome /> -->
   </main>
-  <Pokemons/>
+  <!-- <Pokemons/> -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ NewApp/src/components/Ancora.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import {ref} from 'vue';
+  import axiso from 'axios';
+
+</script>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-2">
+        <label for="name">Entities</label>
+      </div>
+      <select name="select">
+        <option>Valencia</option>
+        <option>Madrid</option>
+        <option>Sevilla</option>
+      </select>
+      <div class="col-2">
+        <label for="name">Address</label>
+      </div>
+      <select name="select">
+        <option>V1111</option>
+        <option>M2222</option>
+        <option>S3333</option>
+      </select>
+    </div>
+  </div>
+</template>
