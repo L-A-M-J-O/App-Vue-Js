0b3673225e435c77af797415694760f6d3c1057c
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-05-14 15:35:37 +0200

    FFA-V0.1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 </script>
-
 <template>
   <header>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/tareas">Tareas</RouterLink>
       </nav>
   </header>
   <RouterView />
---
+++ src/views/TareasView.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { ref, defineProps, reactive, onMounted } from 'vue';
+
+const props = defineProps({
+  id: Number,
+  descripcion: String,
+  completada: Boolean
+})
+interface LabeledValue {
+  id: Number
+  file_id: string;
+  type_id: string;
+  forwarder_id: string;
+}
+
+const value = ref([])
+const address = [
+  {
+    value: 'file_id',
+    label: 'file_id',
+  },
+  {
+    value: 'ForwarderJob_id',
+    label: 'ForwarderJob_id',
+  }
+]
+const file = [
+  {
+    value: 'addres_id',
+    label: 'addres_id',
+  },
+  {
+    value: 'type_id',
+    label: 'type_id',
+  }
+]
+const ruta = ref<LabeledValue[]>([{ 
+    id: 1,
+    file_id: '',
+    type_id: '',
+    forwarder_id: '',
+}])
+
+const agregarRuta = () => {
+  console.log(`agregar`)
+  ruta.value.push({
+    id: ruta.value.length + 1,
+    file_id: '',
+    type_id: '',
+    forwarder_id: '',
+  })
+  console.log(`agregar`, ruta.value.length + 1)
+}
+const eliminarRuta = (id: Number) => {  
+  console.log(`eliminar`)
+  ruta.value = ruta.value.filter(element => element.id !== id);
+}
+const handleChange = () => {
+  console.log(ruta.value)
+}
+onMounted(() => {
+  console.log(ruta.value.length)
+})
+
+</script>
+<template>
+  <section>
+    <h1>Tareas</h1>
+    <form @submit.prevent="handleChange">
+        <button @click="agregarRuta">Agregar tarea</button>
+      <div v-for="element in ruta">
+        <button @click="eliminarRuta(element.id)">Eliminar</button>
+        <el-select v-model="element.file_id" filterable placeholder="Select">
+          <el-option
+            v-for="item in address"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+        <el-select v-model="element.type_id" filterable placeholder="Select">
+          <el-option
+            v-for="item in file"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+        <el-select v-model="element.forwarder_id" filterable placeholder="Select">
+          <el-option
+            v-for="item in file"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </div>
+      <button type="submit">Save</button>
+    </form>
+  </section>
+</template>
